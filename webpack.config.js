73e73985a3833ff48a726adb72c96a791df80262
jalentwright <jalen_t_wright@yahoo.com>
2024-05-26 09:56:24 -0500

    added fonts to text

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,4 +7,18 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: "body",
+            scriptLoading: "defer",
+            minify: "false",
+        }),
+    ],
 };
+
+
+
+
